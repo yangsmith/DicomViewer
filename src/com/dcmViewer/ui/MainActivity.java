79e8a740844339c902b302f066b9f88b717831f4
syy <1172069959@qq.com>
2015-09-01 23:15:50 +0800

    手势识别

--- src/com/dcmViewer/ui/MainActivity.java
+++ src/com/dcmViewer/ui/MainActivity.java
@@ -1,17 +1,153 @@
 package com.dcmViewer.ui;
 
 import com.dicomViewer.R;
+import com.dcmViewer.GestureDetector.MoveGestureDetector;
+import com.dcmViewer.GestureDetector.RotateGestureDetector;
+import com.dcmViewer.GestureDetector.ShoveGestureDetector;
 
 import android.app.Activity;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnTouchListener {
+
+	private Matrix mMatrix = new Matrix();
+	private float mScaleFactor = .4f;
+	private float mRotationDegrees = 0.f;
+	private float mFocusX = 0.f;
+	private float mFocusY = 0.f;
+	private int mAlpha = 255;
+	private int mImageHeight, mImageWidth;
+
+	private ScaleGestureDetector mScaleDetector;
+	private RotateGestureDetector mRotateDetector;
+	private MoveGestureDetector mMoveDetector;
+	private ShoveGestureDetector mShoveDetector;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mainactivity);
+
+		// Determine the center of the screen to center 'earth'
+		Display display = getWindowManager().getDefaultDisplay();
+		mFocusX = display.getWidth() / 2f;
+		mFocusY = display.getHeight() / 2f;
+
+		// Set this class as touchListener to the ImageView
+		ImageView view = (ImageView) findViewById(R.id.dcmview);
+		view.setOnTouchListener(this);
+
+		// Determine dimensions of 'earth' image
+		Drawable d = this.getResources().getDrawable(R.drawable.ic_launcher);
+		mImageHeight = d.getIntrinsicHeight();
+		mImageWidth = d.getIntrinsicWidth();
+
+		// View is scaled and translated by matrix, so scale and translate
+		// initially
+		float scaledImageCenterX = (mImageWidth * mScaleFactor) / 2;
+		float scaledImageCenterY = (mImageHeight * mScaleFactor) / 2;
+
+		mMatrix.postScale(mScaleFactor, mScaleFactor);
+		mMatrix.postTranslate(mFocusX - scaledImageCenterX, mFocusY
+				- scaledImageCenterY);
+		view.setImageMatrix(mMatrix);
+
+		// Setup Gesture Detectors
+		mScaleDetector = new ScaleGestureDetector(getApplicationContext(),
+				new ScaleListener());
+		mRotateDetector = new RotateGestureDetector(getApplicationContext(),
+				new RotateListener());
+		mMoveDetector = new MoveGestureDetector(getApplicationContext(),
+				new MoveListener());
+		mShoveDetector = new ShoveGestureDetector(getApplicationContext(),
+				new ShoveListener());
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+
+		mScaleDetector.onTouchEvent(event);
+		mRotateDetector.onTouchEvent(event);
+		mMoveDetector.onTouchEvent(event);
+		mShoveDetector.onTouchEvent(event);
+
+		float scaledImageCenterX = (mImageWidth * mScaleFactor) / 2;
+		float scaledImageCenterY = (mImageHeight * mScaleFactor) / 2;
+
+		mMatrix.reset();
+		mMatrix.postScale(mScaleFactor, mScaleFactor);
+		mMatrix.postRotate(mRotationDegrees, scaledImageCenterX,
+				scaledImageCenterY);
+		mMatrix.postTranslate(mFocusX - scaledImageCenterX, mFocusY
+				- scaledImageCenterY);
+
+		ImageView view = (ImageView) v;
+		view.setImageMatrix(mMatrix);
+		view.setAlpha(mAlpha);
+
+		return true;
+	}
+
+	private class ScaleListener extends
+			ScaleGestureDetector.SimpleOnScaleGestureListener {
+		@Override
+		public boolean onScale(ScaleGestureDetector detector) {
+			mScaleFactor *= detector.getScaleFactor(); // scale change since
+														// previous event
+
+			// Don't let the object get too small or too large.
+			mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+
+			return true;
+		}
+	}
+
+	private class RotateListener extends
+			RotateGestureDetector.SimpleOnRotateGestureListener {
+		@Override
+		public boolean onRotate(RotateGestureDetector detector) {
+			mRotationDegrees -= detector.getRotationDegreesDelta();
+			return true;
+		}
+	}
+
+	private class MoveListener extends
+			MoveGestureDetector.SimpleOnMoveGestureListener {
+		@Override
+		public boolean onMove(MoveGestureDetector detector) {
+			PointF d = detector.getFocusDelta();
+			mFocusX += d.x;
+			mFocusY += d.y;
+
+			// mFocusX = detector.getFocusX();
+			// mFocusY = detector.getFocusY();
+			return true;
+		}
+	}
+
+	private class ShoveListener extends
+			ShoveGestureDetector.SimpleOnShoveGestureListener {
+		@Override
+		public boolean onShove(ShoveGestureDetector detector) {
+			mAlpha += detector.getShovePixelsDelta();
+			if (mAlpha > 255)
+				mAlpha = 255;
+			else if (mAlpha < 0)
+				mAlpha = 0;
+
+			return true;
+		}
 	}
 
 }
